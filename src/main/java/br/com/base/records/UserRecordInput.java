067b7496caa9f624e2cd86efd439637343a77124
Eduardo William Karpinski Priester <karpinskipriester@gmail.com>
2024-07-02 21:22:28 -0300

    Corrigindo pequeno bug

--- src/main/java/br/com/base/records/UserRecordInput.java
+++ src/main/java/br/com/base/records/UserRecordInput.java
@@ -1,14 +1,10 @@
 package br.com.base.records;
 
-import org.springframework.web.multipart.MultipartFile;
-
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record UserRecordInput(
 		@NotBlank String name,
 		@NotBlank @Email String email,
-		@NotBlank String password,
-		@NotNull Boolean isAdmin,
-		@NotNull MultipartFile image) {}
+		@NotBlank @Size(min = 8, max = 32) String password) {}
