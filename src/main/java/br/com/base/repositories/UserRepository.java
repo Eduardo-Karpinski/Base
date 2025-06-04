e9b999a4eb80b635a3bef1f0d0dca0ab6232aa38
Eduardo William Karpinski Priester <45966684+Eduardo-Karpinski@users.noreply.github.com>
2025-06-04 16:55:30 -0300

    update properties file

--- src/main/java/br/com/base/repositories/UserRepository.java
+++ src/main/java/br/com/base/repositories/UserRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.base.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
 	Boolean existsByEmail(String email);
 	Optional<User> findByEmail(String email);
-	
 }
